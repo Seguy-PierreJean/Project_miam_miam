18902dfb80c2a1390ef66e5b1992e4f63fc677a2
PascalFerret <ferretpascal46@gmail.com>
2020-06-29 14:17:54 +0200

    Ajout d'un main dans test

--- src/main/java/fr/ldnr/group3/Test.java
+++ src/main/java/fr/ldnr/group3/Test.java
@@ -1,6 +1,14 @@
 package fr.ldnr.group3;
 
 public class Test {
+	
+	
+	public static void main (String[] args) {
+		Test t = new Test();
+		
+		t.test();
+	}
+	
 	public void test() {
 		System.out.println("Ceci est un test de PJ");
 	}
